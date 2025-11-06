738cabd8b2e47f3da60e09f34ff927dc89389d38
Frank Spitzock <157513226+FSpitzock@users.noreply.github.com>
2025-11-05 23:29:52 -0500

    some navigation pushes

--- my-react-js-app/src/App.jsx
+++ my-react-js-app/src/App.jsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import './index.css'
+import Footer from './components/Layout/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
-// <BrowserRouter basename="/Diamond_Data_Baseball_Stat_Tracker/">
+
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/Stats" element={<Stats/>}/>
+        {/* 🧭 404 Catch-All Route */}
+          <Route path="*" element={<NotFound />} />
+    </Routes>
+    <Footer/> 
+    </BrowserRouter>
+     
+    
     </>
   )
 }
