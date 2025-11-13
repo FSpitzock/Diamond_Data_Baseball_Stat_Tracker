856b20a88eaad80f843ff678fc1841a5627d36a5
Frank Spitzock <157513226+FSpitzock@users.noreply.github.com>
2025-11-12 19:46:05 -0500

    updated vite config so that local host directs to home page

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Diamond_Data_Baseball_Stat_Tracker/',
+  base: '/',
   plugins: [
     react(),
     tailwindcss(),
